0d3344ccd4138aa29ba66cb14b56e0f1a662758d
github username <github email address>
2020-06-01 16:52:48 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CatComponent from './CatComponent.js'
-import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
-import MouseComponent from './MouseComponent.js'
+import CatComponent from './CatComponent'
+import { GraceHopperQuoteComponent } from './GraceHopperQuoteComponent'
+import MouseComponent from './MouseComponent'
 
 class App extends Component {
 	render() {
