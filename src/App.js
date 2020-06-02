613b84b5b7dfe69759e8c0faa80a92ad80212e23
lightfire224 <elias.taveras@columbia.edu>
2020-06-01 20:30:34 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent';
+import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent';
+import MouseComponent from './MouseComponent'
+//when youre importing default, no curly braces
+// you can also change the name of the export default to be
+// anything you want when importing
+
+//if not importing default, use curly braces and 
+// export name must match import name
+
+//when you dont export default you are creating a hash
+// the key is the exports name and the value is the
+// value of the key AKA all of the exports code
 
 class App extends Component {
 	render() {
@@ -7,7 +20,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<MouseComponent/>
 			</div>
 		);
 	}
